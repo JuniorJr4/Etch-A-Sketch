f2cdad5fec3a2fe01103fa0d4c2e145abb6f867f
Clint G <cgardner4@gmail.com>
2022-10-06 18:25:05 +0200

    Improved redrawing grid, but is still slow on larger grids

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const gridContainer = document.querySelector('.grid-container');
 
 function makeGrid(x) {
+    
     gridContainer.style.gridTemplateColumns = `repeat(${x}, 1fr)`
     gridContainer.style.gridTemplateRows = `repeat(${x}, 1fr)`
     for (let rows = 0; rows < x * x; rows++) {
@@ -8,8 +9,6 @@ function makeGrid(x) {
         newDiv.classList.add('square');
         gridContainer.appendChild(newDiv);
         const square = document.querySelectorAll('.square');
-
-        console.log(square);
         square.forEach(sq => {
             sq.addEventListener("mouseenter", function (e) {
                 e.target.style.backgroundColor = "black";
@@ -25,8 +24,6 @@ makeGrid(4);
 function reSizeGrid() {
     let newSize = prompt("How many squares per side do you want? Input a number less than 64.")
     const square = document.querySelectorAll('.square');
-    square.forEach(rem => {
-        rem.remove();
-    });
+    gridContainer.innerHTML = '';
     makeGrid(newSize);
 }
